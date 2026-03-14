00bfb7281625997087173a14b859edbd41c0ee0a
Hashimali-khan <hashimali-khan@example.com>
2026-03-14 08:01:59 +0500

    leared basic concepts of form handling and two way binding in react

---
+++ form-handling/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const submission = (e) => {
+    e.preventDefault()
+    console.log('form submitted')
+  }
+
+  const [title, settitle] = useState("")
+
+  return (
+    <div>
+      <form onSubmit={(e) => 
+        {submission(e)}
+        }>
+           <input type="text"
+           value={title} onChange={e=>{
+            settitle(e.target.value)
+           }} placeholder='enter your name' />
+        <button type='submit'>Submit</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default App

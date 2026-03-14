a9b59fe15e0cb9b83903b903254eca7589eacd11
Hashimali-khan <hashimali-khan@example.com>
2026-03-14 08:04:02 +0500

    updated two way binding

--- form-handling/src/App.jsx
+++ form-handling/src/App.jsx
@@ -4,7 +4,8 @@ import { useState } from 'react'
 const App = () => {
   const submission = (e) => {
     e.preventDefault()
-    console.log('form submitted')
+    console.log('form submitted by {title}')
+    settitle("")
   }
 
   const [title, settitle] = useState("")

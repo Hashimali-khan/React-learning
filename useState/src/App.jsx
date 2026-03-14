8f38d5820d7dcfa6bcf55acb3c860c1a885c4e99
Hashimali-khan <hashimali-khan@example.com>
2026-03-14 06:37:04 +0500

    basic use of useState using useState snippet

--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
 const App = () => {
+
+  
+
+const [a, seta] = useState(10);
+let change=()=>{
+  seta(30);
+  console.log(a);
+}
+
   return (
     <div>
+      <h1>hi value of a is {a}</h1>
+      <button onClick={change}>click me</button>
       
     </div>
   )

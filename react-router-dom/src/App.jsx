c6a129163ef044230a446a430616f6a65ed1f87d
Hashimali-khan <hashimalikhan2020@gmail.com>
2026-03-18 22:54:10 +0500

    basic routing setup

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+        <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
